7f4d74142e3dba033afcfc45c9bd40aa4d2b442f
sheryel <sheryel57@gmail.com>
2023-06-07 00:53:31 +0800

    updated

--- botthing.py
+++ botthing.py
@@ -1,12 +1,14 @@
 from base64 import b64decode
-
-tokenfile = open('TOKEN', 'r')
-__THING__ = tokenfile.read()
-__THING__ = b64decode(__THING__).decode('utf-8')
-
 import discord
 from discord.ext import commands
-from random import randint
+from random import randint, random
+
+
+tokenfile = open('TOKEN', 'r')
+data = tokenfile.readlines()
+__THING__ = b64decode(data[0]).decode('utf-8')
+sheryel = int(b64decode(data[1]).decode('utf-8'))
+awex = int(b64decode(data[2]).decode('utf-8'))
 
 intents = discord.Intents.all()
 messages = ['uwu', 'x3', ':3', 'rawr']
@@ -19,25 +21,22 @@ bot =  commands.Bot(command_prefix='$',intents=intents)
 
 @bot.event
 async def on_message(message):
-    if message.author.id == 973943523655164032:
+    if message.author.id == sheryel:
         if(message.content == 'kiss boyfie' or message.content == 'kiss awest' or message.content == 'kiss awext'):
-            await message.channel.send("*kisses* <@590880459660066818> " + messages[randint(0, 3)])
+            await message.channel.send("*kisses* <@" + str(awex) + "> " + messages[randint(0, 3)])
 
 
         if(message.content == 'i wuv boyfie' or message.content == 'i wuv awest' or message.content == 'i wuv awext'):
-            await message.channel.send("i wuv you <@590880459660066818> " + messages[randint(0, 3)])
+            await message.channel.send("i wuv you <@" + str(awex) + "> " + messages[randint(0, 3)])
 
 
         if(message.channel.id == 1115319072314372177 and 'rate' in message.content):
-            await message.channel.send("<@590880459660066818> " + messages[randint(0, 3)])
+            await message.channel.send("<@" + str(awex) + "> " + messages[randint(0, 3)])
 
 
         if(message.content in real_spellings):
-            try:
-                await message.delete()
-                await message.channel.send("# stop playing real <@973943523655164032>")
-            except Exception:
-                await message.channel.send("who took my permission grr")
+            await message.delete()
+            await message.channel.send("# stop playing real <@" + str(sheryel) + ">")
 
         if((message.content.lower()).strip('!@#$%^&*(){}|:"<>?\'1234567890-=[]\;,./') in 'hihellohellphii'):
             await message.reply('hi')
